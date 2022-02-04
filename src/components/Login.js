79354d27055fb09f8b8cff25fe545b167829bdcf
alorentiar <alorentiar03@gmail.com>
2022-02-04 22:02:06 +0700

    update login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 export const Login = () => {
-  return <div>THIS IS LOGIN PAGE</div>;
+  return (
+    <div className="form">
+      <label className="label">Username :</label>
+      <input type="text" name="username" placeholder="Username"></input>
+      <label className="label">Password :</label>
+      <input type="password" name="password" placeholder="Password"></input>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Login;
